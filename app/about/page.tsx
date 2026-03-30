2bd4c459917e7693064c8c408f3769485fe40841
Arkyadeep pal <arkyadeep.pal2017@gmail.com>
2026-03-30 11:43:36 +0530

    Initialize commit for app/about/page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Template, { Heading, SubHeading } from "../../src/components/Template";
+
+export default function AboutPage() {
+  return (
+    <Template>
+      <Heading>
+        You should Know Us
+      </Heading>
+      <SubHeading> This is a Next.js app built to explore smooth page transitions. Simple,
+      fast, and ready for whatever comes next. </SubHeading>
+    </Template>
+  );
+}
