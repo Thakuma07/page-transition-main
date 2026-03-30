421f82e56c27910385470fbf9a70ee0ea6369c60
Arkyadeep pal <arkyadeep.pal2017@gmail.com>
2026-03-30 11:43:36 +0530

    Initialize commit for app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Template, { Heading, SubHeading } from "../src/components/Template";
+
+const page = () => {
+  return (
+    <Template>
+      <Heading>Livin&apos; The Dream</Heading>
+      <SubHeading> A simple foundation for beautiful page transitions. Clean typography,
+      centered content, and room to grow. </SubHeading>
+    </Template>
+  );
+};
+
+export default page;
