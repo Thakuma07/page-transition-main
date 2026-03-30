ff4c7dc1740815a47c7c378bd0dbe44b09392c0d
Arkyadeep pal <arkyadeep.pal2017@gmail.com>
2026-03-30 11:43:37 +0530

    Initialize commit for app/work/page.tsx

---
+++ app/work/page.tsx
@@ -0,0 +1,10 @@
+import Template, { Heading, SubHeading } from "../../src/components/Template";
+
+export default function WorkPage() {
+  return (
+    <Template>
+      <Heading>See What We Do</Heading>
+      <SubHeading> Projects and experiments live here. More coming soon. </SubHeading>
+    </Template>
+  );
+}
