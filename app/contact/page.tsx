086d07e38b4510e27f657450caf0393d9a57aec5
Arkyadeep pal <arkyadeep.pal2017@gmail.com>
2026-03-30 11:43:36 +0530

    Initialize commit for app/contact/page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Template, { Heading, SubHeading } from "../../src/components/Template";
+
+export default function ContactPage() {
+  return (
+    <Template>
+      <Heading> Know Us Better?</Heading>
+      <SubHeading> Get in touch. Say hello. Or just wave from afar. </SubHeading>
+    </Template>
+  );
+}
